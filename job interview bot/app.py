e279109ca2f34c8a0734e2ca4c451dc7a6e7fbdf
sajith007423 <154319727+sajith007423@users.noreply.github.com>
2023-12-19 14:46:44 +0530

    Add files via upload

---
+++ job interview bot/app.py
@@ -0,0 +1,121 @@
+import streamlit as st
+from streamlit_chat import message
+from dotenv import load_dotenv
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader,CSVLoader,JSONLoader,TextLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.llms import HuggingFaceHub
+import torch
+print("Torch version:",torch.__version__)
+print("Is CUDA enabled?",torch.cuda.is_available())
+load_dotenv()
+# Function to load documents 
+def load_documents():
+    loader = DirectoryLoader('data/', glob="*.pdf", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+def load_documents2():
+    loader = DirectoryLoader('data/', glob="*.csv", loader_cls=CSVLoader)
+    documents = loader.load()
+    return documents
+
+def load_documents3():
+    loader = DirectoryLoader('data/', glob='*.json', show_progress=True, loader_cls=TextLoader)
+    documents = loader.load()
+    return documents
+
+# Function to split text into chunks
+def split_text_into_chunks(documents):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    text_chunks = text_splitter.split_documents(documents)
+    return text_chunks
+
+# Function to create embeddings
+def create_embeddings():
+    #embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': "cuda"})
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2", model_kwargs={'device': "cuda"})
+    return embeddings
+
+# Function to create vector store
+def create_vector_store(text_chunks, embeddings):
+    vector_store = FAISS.from_documents(text_chunks, embeddings)
+    return vector_store
+
+# Function to create LLMS model
+def create_llms_model():
+    #llm = CTransformers(model="mistral-7b-instruct-v0.1.Q4_K_M.gguf", config={'max_new_tokens': 128, 'temperature': 0.1,'gpu_layers':1})
+    #llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":1, "max_length":1000})
+    llm = HuggingFaceHub(repo_id="mistralai/Mistral-7B-Instruct-v0.2", model_kwargs={"temperature":1, "max_length":1000,'gpu_layers':4})
+    #llm = HuggingFaceHub(repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1", model_kwargs={"temperature":0.01, "max_length":1000,'gpu_layers':4})
+    return llm
+
+# Initialize Streamlit app
+st.title("Job Interview Prep ChatBot")
+st.title("Personalized Job Success Friend")
+st.markdown('<style>h1{color: orange; text-align: center;}</style>', unsafe_allow_html=True)
+st.subheader('Get Your Desired Job 💪')
+st.markdown('<style>h3{color: pink; text-align: center;}</style>', unsafe_allow_html=True)
+
+# loading of documents
+documents = load_documents3()
+
+# Split text into chunks
+text_chunks = split_text_into_chunks(documents)
+
+# Create embeddings
+embeddings = create_embeddings()
+
+# Create vector store
+vector_store = create_vector_store(text_chunks, embeddings)
+
+# Create LLMS model
+llm = create_llms_model()
+
+# Initialize conversation history
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["Hello! Ask me anything about 🤗"]
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = ["Hey! 👋"]
+
+# Create memory
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+# Create chain
+chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type='stuff',
+                                              retriever=vector_store.as_retriever(search_kwargs={"k": 2}),
+                                              memory=memory)
+
+# Define chat function
+def conversation_chat(query):
+    result = chain({"question": query, "chat_history": st.session_state['history']})
+    st.session_state['history'].append((query, result["answer"]))
+    return result["answer"]
+
+# Display chat history
+reply_container = st.container()
+container = st.container()
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_input("Question:", placeholder="Ask about your Job Interview", key='input')
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        output = conversation_chat(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+
+if st.session_state['generated']:
+    with reply_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+            message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
---
+++ mainframe/pages/1_contacts.py
@@ -0,0 +1,3 @@
+import streamlit as st
+if st.session_state["authentication_status"]:
+    st.title("contacts")
